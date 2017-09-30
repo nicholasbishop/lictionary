4159d004089f1498d3006549fa267bccec9c7716
Nicholas Bishop <nicholasbishop@gmail.com>
2017-09-30 19:03:09 -0400

    Inherit from MutableSequence

--- lictionary/__init__.py
+++ lictionary/__init__.py
@@ -1,15 +1,18 @@
-class Lictionary(object):
+import collections
+
+class Lictionary(collections.MutableSequence):
     __slots__ = ('_items',)
 
     def __init__(self, items=None):
+        super(Lictionary, self).__init__()
         self._items = items or []
-
-    def append(self, item):
-        self._items.append(item)
 
     @property
     def items(self):
         return self._items
+    
+    def insert(self, index, item):
+        self._items.insert(index, item)
 
     def __getitem__(self, key):
         if isinstance(key, int):
@@ -21,6 +24,18 @@ class Lictionary(object):
                 if isinstance(ele, tuple) and len(ele) == 2 and ele[0] == key:
                     return ele[1]
             raise KeyError('not found')
+
+    def __setitem__(self, key, value):
+        if isinstance(key, int):
+            self._items[key] = value
+        else:
+            raise NotImplementedError()
+
+    def __delitem__(self, key):
+        raise NotImplementedError()
+
+    def __len__(self):
+        return len(self._items)
 
     def get(self, key, default=None):
         try:
