eba825b645303f65a8d3f2d07ddd99246c04e06c
Nicholas Bishop <nicholasbishop@gmail.com>
2017-09-30 18:37:12 -0400

    Initial commit

---
+++ tests/test_lictionary.py
@@ -0,0 +1,35 @@
+import unittest
+
+from lictionary import Lictionary
+
+class TestLictionary(unittest.TestCase):
+    def test_append(self):
+        lict = Lictionary()
+        lict.append(1)
+        self.assertEqual(lict.items, [1])
+
+    def test_numeric_lookup(self):
+        lict = Lictionary([123])
+        self.assertEqual(lict[0], 123)
+        self.assertEqual(lict.get(0), 123)
+
+    def test_string_lookup(self):
+        lict = Lictionary([('key', 123)])
+        self.assertEqual(lict['key'], 123)
+        self.assertEqual(lict.get('key'), 123)
+
+    def test_duplicate_key(self):
+        lict = Lictionary([('key', 123),
+                           ('key', 456)])
+        self.assertEqual(lict[0], ('key', 123))
+        self.assertEqual(lict[1], ('key', 456))
+        self.assertEqual(lict['key'], 456)
+
+    def test_not_found(self):
+        lict = Lictionary()
+        self.assertIs(lict.get(0), None)
+        self.assertIs(lict.get('key'), None)
+        with self.assertRaises(IndexError):
+            lict[0]
+        with self.assertRaises(KeyError):
+            lict['key']
